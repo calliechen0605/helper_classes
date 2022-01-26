ff9f383627f800db8d0064546a704d843a97d29b
calliechen0605 <94504106+calliechen0605@users.noreply.github.com>
2022-01-25 21:28:56 -0500

    Add files via upload

--- services.py
+++ services.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 import bisect
-
 import calendar
 import datetime
 
+from matplotlib import colors
 
 class VolService(object):
 
@@ -100,3 +98,125 @@ class DateService(object):
     def get_prev_expiry(cls, date=datetime.datetime.today().date()):
         expiries = cls.get_expiry_dates_for_year(date.year - 1) + cls.get_expiry_dates_for_year(date.year)
         return expiries[bisect.bisect_left(expiries, date) - 1]
+
+
+class DfFormatService(object):
+
+    @classmethod
+    def background_gradient(cls, s, m, M, cmap='Greens', low=0, high=0):
+        rng = M - m
+        norm = colors.Normalize(
+            m - (rng * low),
+            M + (rng * high),
+        )
+        normed = norm(s.values)
+        c = [colors.rgb2hex(x) for x in plt.cm.get_cmap(cmap)(normed)]
+        return ['background-color: %s' % color for color in c]
+
+'''
+df = pd.DataFrame([[3, 2, 10, 4], [20, 1, 3, 2], [5, 4, 6, 1]])
+
+df.style.apply(DfFormatService.background_gradient,
+               cmap='Greens',
+               m=df.min().min(),
+               M=df.max().max(),
+               low=0,
+               high=0.2)
+'''
+
+#draw subplot
+'''
+fig = make_subplots(
+    rows=2, 
+    cols=1,
+    vertical_spacing= 0.1,
+    subplot_titles=("Realised Vol", "Random Chart")
+)
+
+fig.add_trace(
+    go.Scatter(
+        x = close_df['Date'],
+        y = close_df['AAPL_realized_vol'],
+        name="AAPL_realized_vol",
+    ),
+        row=1, 
+        col=1,
+
+)
+
+fig.add_trace(
+    go.Bar(
+        x = close_df['Date'],
+        y = close_df['GOOGL_realized_vol'],
+        name = "GOOGL_realized_vol",
+),
+        row=1, 
+        col=1,
+)
+
+
+fig.add_trace(
+    go.Line(x=[20, 30, 40], 
+            y=[50, 60, 70],
+            name = "Random Test",
+           ),
+    
+    row=2, col=1,
+)
+
+xaxes_layout = {
+    'showline' : True,
+    'showgrid' : True,
+    'linecolor' :'black',
+    'tickfont' : {'family' : 'Calibri'}
+}
+
+yaxes_pct_layout = {
+    'showline' : True,
+    'showgrid' : True,
+    'linecolor' : 'black',
+    'tickfont' : {'family' : 'Times New Roman'},
+    'tickformat' : '.2%',
+}
+
+yaxes_number_layout = {
+    'showline' : True,
+    'showgrid' : True,
+    'linecolor' : 'black',
+    'tickfont' : {'family' : 'Times New Roman'},
+    'tickformat' : '.2f',
+}
+
+fig.update_layout(
+    
+    height=1000, 
+    width=800,
+        
+    title ={
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top',
+    },
+
+    legend_title_text=' ',
+    
+    xaxis1 = xaxes_layout, 
+    xaxis2 = xaxes_layout,
+            
+
+    yaxis1 = yaxes_pct_layout,
+    yaxis2 = yaxes_number_layout,
+
+    legend = {
+        'y' : 1.1,
+        'x' : 0.1,
+        'orientation' : "h",
+    },
+    
+    plot_bgcolor = 'white'
+)
+
+fig.show()
+
+'''
